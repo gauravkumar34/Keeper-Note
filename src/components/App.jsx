a788af5963d9c280917bfce4ae1f0b663c8fb944
ictrl <49734163+gauravkumar34@users.noreply.github.com>
2020-08-28 03:52:44 +0530

    add more notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-
-      <Note />
+      {notes.map((noteTerm) => (
+        <Note
+          key={noteTerm.key}
+          title={noteTerm.title}
+          content={noteTerm.content}
+        />
+      ))}
+      ;
       <Footer />
     </div>
   );
